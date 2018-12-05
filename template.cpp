aed275a61161fbd1987f5134c42e7a1f8ae0ee68
Henrique-Brito <henriquejbrito@gmail.com>
2018-12-04 22:19:49 -0200

    Add template

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define BUFF ios::sync_with_stdio(false)
+
+typedef long long int ll;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+const int MAX = (int)10e5+10;
+
+int main(int argc, char const *argv[]) {
+
+  BUFF;
+
+
+
+  return 0;
+}
