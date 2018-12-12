72029a4ff8d802cf046c37597d00a540941099e7
Henrique-Brito <henriquejbrito@gmail.com>
2018-12-12 18:46:21 -0200

    Update Exp. Binaria

---
+++ Teoria dos números/exp_binaria.cpp
@@ -0,0 +1,21 @@
+
+// Exponenciação Binária
+
+// (A^B) % MOD
+
+// Complexidade - O(log(b))
+
+typedef long long ll;
+
+ll binpow(ll a, ll b, ll mod){
+  a %= mod;
+  ll ret = 1;
+  while(b > 0){
+    if(b & 1){
+      ret = (ret*a) % mod;
+    }
+    a = (a*a) % mod;
+    b = b >> 1;
+  }
+  return ret;
+}
