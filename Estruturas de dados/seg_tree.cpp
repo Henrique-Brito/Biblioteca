d9d6cf2ce1453ca73a1acaea84f86cfd20fa3244
Henrique Brito <henriquejbrito@gmail.com>
2019-06-16 11:00:31 -0300

    Update SegTree

--- Estruturas de dados/seg_tree.cpp
+++ Estruturas de dados/seg_tree.cpp
@@ -10,40 +10,56 @@
 // Update -> O(log(n))
 
 const int MAX = (int)2e5 + 10;
-int n, v[MAX], seg[4*MAX];
 
-void build(int at, int b, int e){
-  if( b==e ){
-    seg[at] = v[b];
-	  return;
+namespace seg{
+
+  int n;
+  int vec[MAX];
+  ll seg[4*MAX];
+
+  ll make_node(int v){
+    return v;
   }
-  int m = (b+e)/2;
-  build(2*at, b, m);
-  build(2*at +1, m+1, e);
-  seg[at] = seg[2*at] + seg[2*at +1];
-}
-
-ll query(int at, int b, int e, int qb, int qe){
-  if( b>e || qb>e || qe<b ){
+  ll make_neutro(){
     return 0;
   }
-  if( qb<=b && e<=qe ){
-    return seg[at];
+  ll combina(ll a, ll b){
+    return a+b;
   }
-  int m = (b+e)/2;
-  return query(2*at, b, m, qb, qe) + query(2*at +1, m+1, e, qb, qe);
-}
 
-void update(int at, int b, int e, int pos, int x){
-  if( b==e ){
-    seg[at] = x;
-    return;
+  void build(int at=1, int l=0, int r=n-1){
+    if( l==r ){
+      seg[at] = make_node(vec[l]);
+  	  return;
+    }
+    int m = (l+r)/2;
+    build(2*at, l, m);
+    build(2*at +1, m+1, r);
+    seg[at] = combina(seg[2*at], seg[2*at +1]);
   }
-  int m = (b+e)/2;
-  if( pos <= m ){
-    update(2*at, b, m, pos, x);
-  }else{
-    update(2*at +1, m+1, e, pos, x);
+
+  ll query(int ql, int qr, int at=1, int l=0, int r=n-1 ){
+    if( l>r || ql>r || qr<l ){
+      return make_neutro();
+    }
+    if( ql<=l && r<=qr ){
+      return seg[at];
+    }
+    int m = (l+r)/2;
+    return combina(query(ql, qr, 2*at, l, m), query(ql, qr, 2*at +1, m+1, r));
   }
-  seg[at] = seg[2*at] + seg[2*at +1];
+
+  void update(int pos, int x, int at=1, int l=0, int r=n-1){
+    if( l==r ){
+      seg[at] = make_node(x);
+      return;
+    }
+    int m = (l+r)/2;
+    if( pos <= m ){
+      update(pos, x, 2*at, l, m);
+    }else{
+      update(pos, x, 2*at +1, m+1, r);
+    }
+    seg[at] = combina(seg[2*at], seg[2*at +1]);
+  }
 }
